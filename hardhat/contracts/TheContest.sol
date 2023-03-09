b5e430185ba2790165118eefd6328663e16f17e6
RaynHarr <ryanhill86@gmail.com>
2023-03-09 11:06:43 -0500

    pushing first code

---
+++ hardhat/contracts/TheContest.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+import "usingtellor/contracts/UsingTellor.sol";
+
+interface IERC20 {
+    function transfer(address _to, uint256 _amount) external returns(bool);
+    function transferFrom(address _from, address _to, uint256 _amount) external returns(bool);
+}
+
+contract TheContest is UsingTellor {
+    address public owner;
+    uint256 public startDeadline; // when the contest starts
+    uint256 public endDeadline;
+    uint256 public wager; // contestant's initial stake
+    IERC20 public token;
+    uint256 public ownerBalance;
+    uint256 public pot;
+    uint256 public protocolFee;
+    uint256 public remainingCount;
+    uint256 public reportingWindow = 1 days; 
+    bytes queryData = abi.encode("TwitterContestV1", abi.encode(bytes("")));
+    bytes32 queryId = keccak256(queryData);
+    string[] private _handles;
+    
+    mapping(address => Member) public members;
+    mapping(string => address) public handleToAddress;
+
+    struct Member {
+        bool inTheRunning;
+        bool claimedFunds;
+        string handle;
+    }
+
+    constructor(
+        address payable _tellor, 
+        address _token, 
+        uint256 _wager,
+        uint256 _startDeadlineDays,
+        uint256 _endDeadlineDays,
+        uint256 _protocolFee)
+        UsingTellor(_tellor) {
+        startDeadline = block.timestamp + _startDeadlineDays * 1 days;
+        endDeadline = startDeadline + _endDeadlineDays * 1 days;
+        wager = _wager;
+        token = IERC20(_token);
+        owner = msg.sender;
+        protocolFee = _protocolFee;
+    }
+
+    function register(string memory _handle) public {
+        require(bytes(_handle).length > 0, "Handle cannot be empty");
+        Member storage _member = members[msg.sender];
+        require(token.transferFrom(msg.sender, address(this), wager+protocolFee), "Wager + fee transfer failed");
+        require(!_member.inTheRunning, "Account already registered");
+        require(block.timestamp < startDeadline, "Contest already started");
+        require(handleToAddress[_handle] == address(0), "Handle already registered");
+        pot += wager;
+        ownerBalance += protocolFee;
+        remainingCount++;
+        _member.handle = _handle;
+        _member.inTheRunning = true;
+        handleToAddress[_handle] = msg.sender;
+        _handles.push(_handle);
+    }
+
+    function claimLoser(uint256 _index) public {
+        require(remainingCount > 1, "Only one user left");
+        require(block.timestamp > startDeadline, "Contest has not started");
+        require(block.timestamp < endDeadline + reportingWindow, "Contest has ended");
+        uint256 _timestampRetrieved = getTimestampbyQueryIdandIndex(queryId, _index);
+        require(_timestampRetrieved > 0, "No data found");
+        require(_timestampRetrieved + 12 hours < block.timestamp, "Oracle dispute period has not passed");
+        bytes memory _valueRetrieved = retrieveData(queryId, _timestampRetrieved);
+        string memory _handle = abi.decode(_valueRetrieved, (string));
+        address _user = handleToAddress[_handle];
+        require(members[_user].inTheRunning, "User is not in the running");
+        members[_user].inTheRunning = false;
+        remainingCount--;
+    }
+
+    function claimFunds() public {
+        Member storage _member = members[msg.sender];
+        require(_member.inTheRunning, "not a valid participant");
+        require(!_member.claimedFunds, "funds already claimed");
+        // Wouddn't the OR condition result in a bug if the contest was over, but there was only one participant left who'd kept their streak?
+        require(block.timestamp > endDeadline + reportingWindow || remainingCount == 1, "Game still active");
+        _member.claimedFunds = true;
+        token.transfer(msg.sender, pot / remainingCount);
+    }
+
+    function ownerClaim() public {
+        require(msg.sender == owner, "Only owner can claim");
+        token.transfer(owner, ownerBalance);
+        ownerBalance = 0;
+    }
+
+    // Getters
+    function getMemberInfo(address _user) public view returns(Member memory) {
+        return members[_user];
+    }
+
+    function getStartDeadline() public view returns(uint256) {
+        return startDeadline;
+    }
+
+    function getHandlesList() public view returns(string[] memory) {
+        return _handles;
+    }
+}
---
+++ hardhat/test/functionTests-TheContest.js
@@ -0,0 +1,224 @@
+const {expect,assert} = require("chai");
+const {ethers} = require("hardhat");
+const h = require("./helpers/helpers");
+const web3 = require("web3");
+const {keccak256} = require("@ethersproject/keccak256");
+const { abi, bytecode } = require("usingtellor/artifacts/contracts/TellorPlayground.sol/TellorPlayground.json")
+
+describe("TheContest - Function tests", function() {
+  let tellor;
+  let token;
+  let contest;
+  let accounts;
+  const abiCoder = new ethers.utils.AbiCoder;
+  const START_DEADLINE_DAYS = 1;
+  const END_DEADLINE_DAYS = 100;
+  const PROTOCOL_FEE = h.toWei("10")
+  const WAGER = h.toWei("500")
+  const queryData = abiCoder.encode(["string", "bytes"], ["TwitterContestV1", abiCoder.encode(["bytes"], ["0x"])]);
+  const queryId = keccak256(queryData);
+  console.log("queryId:", queryId)
+  console.log("queryData:", queryData)
+  
+
+  beforeEach(async function() {
+    accounts = await ethers.getSigners();
+    const TellorPlayground = await ethers.getContractFactory(abi, bytecode);
+    tellor = await TellorPlayground.deploy();
+    await tellor.deployed();
+    token = await TellorPlayground.deploy();
+    await token.deployed();
+    const Contest = await ethers.getContractFactory("TheContest");
+    contest = await Contest.deploy(tellor.address, token.address, WAGER, START_DEADLINE_DAYS, END_DEADLINE_DAYS, PROTOCOL_FEE);
+    await contest.deployed();
+  });
+
+  it("constructor", async function() {
+    blocky0 = await h.getBlock()
+    tellorAddressRetrieved = await contest.tellor()
+    assert.equal(tellorAddressRetrieved, tellor.address, "tellor address not set correctly");
+    assert.equal(await contest.startDeadline(), START_DEADLINE_DAYS * 86400 + blocky0.timestamp, "start deadline not set correctly");
+    assert.equal(await contest.endDeadline(), (START_DEADLINE_DAYS + END_DEADLINE_DAYS) * 86400 + blocky0.timestamp, "end deadline not set correctly");
+    assert.equal(await contest.protocolFee(), PROTOCOL_FEE, "protocol fee not set correctly");
+    assert.equal(await contest.wager(), WAGER, "wager not set correctly");
+  });
+
+  it("register", async function() {
+    handle1 = "jjabramsdog"
+    handle2 = "bingo"
+    handle3 = "bongo"
+    // fund accounts
+    bob = accounts[1]
+    alice = accounts[2]
+    ricky = accounts[3]
+    await token.faucet(bob.address)
+    await token.faucet(bob.address)
+    await token.faucet(alice.address)
+    await token.faucet(ricky.address)
+
+    // try to register with no twitter handle included
+    await expect(contest.connect(bob).register("")).to.be.revertedWith("Handle cannot be empty");
+
+    // register successfully
+    balanceBefore = await token.balanceOf(bob.address)
+    await token.connect(bob).approve(contest.address, WAGER + PROTOCOL_FEE)
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 0, "handlesList not empty");
+    await contest.connect(bob).register(handle1)
+    balanceAfter = await token.balanceOf(bob.address)
+    memberInfo = await contest.getMemberInfo(bob.address)
+    assert.equal(memberInfo.handle, handle1, "twitter handle not set correctly");
+    assert.equal(memberInfo.inTheRunning, true, "inTheRunning not set correctly");
+    assert.equal(memberInfo.claimedFunds, false, "claimedFunds not set correctly");
+    assert.equal(balanceBefore - balanceAfter, BigInt(WAGER) + BigInt(PROTOCOL_FEE), "wager and protocol fee not deducted correctly");
+    assert.equal(await contest.remainingCount(), 1, "remainingCount not incremented");
+    assert.equal(await contest.pot(), WAGER, "pot not correct");
+    assert.equal(await contest.handleToAddress(handle1), bob.address, "handle not mapped to address");
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 1, "handlesList not updated");
+    assert.equal(handlesList[0], handle1, "handlesList not updated");
+
+    // try to register with same account
+    await token.connect(bob).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await expect(contest.connect(bob).register(handle2)).to.be.revertedWith("Account already registered");
+    // try to register with same twitter handle
+    await token.connect(alice).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await expect(contest.connect(alice).register(handle1)).to.be.revertedWith("Handle already registered");
+
+    // try to register after start deadline
+    await h.advanceTime(START_DEADLINE_DAYS * 86400 + 1)
+    await token.connect(ricky).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await expect(contest.connect(ricky).register(handle3)).to.be.revertedWith("Contest already started");
+  });
+
+  it("claimLoser", async function() {
+    // register one account
+    await token.faucet(accounts[1].address)
+    await token.connect(accounts[1]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[1]).register("jjabramsdog")
+
+    // try to claim loser with only one account
+    await expect(contest.connect(accounts[1]).claimLoser(1)).to.be.revertedWith("Only one user left");
+
+    // register two more accounts
+    await token.faucet(accounts[2].address)
+    await token.connect(accounts[2]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[2]).register("bingo")
+    await token.faucet(accounts[3].address)
+    await token.connect(accounts[3]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[3]).register("bongo")
+
+    // try to claim loser before contest start
+    await expect(contest.connect(accounts[1]).claimLoser(1)).to.be.revertedWith("Contest has not started");
+
+    // try to claim loser when no oracle value has been submitted. Simulates invalid index input as well
+    await h.advanceTime(START_DEADLINE_DAYS * 86400 + 1) // advance time past contest start
+    await expect(contest.connect(accounts[1]).claimLoser(1)).to.be.revertedWith("No data found");
+
+    // submitValue to tellor oracle signifying someone broke their tweeting streak
+    let loserHandleAsBytes = abiCoder.encode(["string"], ["bongo"])
+    await tellor.submitValue(queryId, loserHandleAsBytes, 0, queryData);
+
+    // try to claim loser before oracle dispute period has elapsed
+    let block = await h.getBlock()
+    let blockTs = block.timestamp
+    await expect(contest.connect(accounts[1]).claimLoser(0)).to.be.revertedWith("Oracle dispute period has not passed");
+
+    // successfully claim loser
+    await h.advanceTime(12 * 3600 + 1) // advance time past oracle dispute period of 12 hours
+    let memberInfoBefore = await contest.getMemberInfo(accounts[3].address)
+    assert.equal(memberInfoBefore.inTheRunning, true, "member should be in the running");
+    await contest.connect(accounts[1]).claimLoser(0)
+    let memberInfo = await contest.getMemberInfo(accounts[3].address)
+    assert.equal(memberInfo.inTheRunning, false, "inTheRunning not set correctly");
+
+    // try to claim loser on account not "in the money"
+    await expect(contest.connect(accounts[1]).claimLoser(0)).to.be.revertedWith("User is not in the running");
+
+    // try to claim loser after contest has ended
+    await h.advanceTime((END_DEADLINE_DAYS + 1) * 86400 + 1)
+    await expect(contest.connect(accounts[1]).claimLoser(0)).to.be.revertedWith("Contest has ended");
+  });
+
+  it("claimFunds", async function() {
+    // register three accounts
+    await token.faucet(accounts[1].address)
+    await token.connect(accounts[1]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[1]).register("jjabramsdog")
+    await token.faucet(accounts[2].address)
+    await token.connect(accounts[2]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[2]).register("bingo")
+    await token.faucet(accounts[3].address)
+    await token.connect(accounts[3]).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await contest.connect(accounts[3]).register("bongo")
+
+    // claim loser on one participant
+    await h.advanceTime(START_DEADLINE_DAYS * 86400 + 1) // advance time past contest start
+    let loserHandleAsBytes = abiCoder.encode(["string"], ["bongo"])
+    await tellor.submitValue(queryId, loserHandleAsBytes, 0, queryData);
+    await h.advanceTime(12 * 3600 + 1) // advance time past oracle dispute period of 12 hours
+    await contest.connect(accounts[1]).claimLoser(0)
+
+    // try to claim funds as non-participant
+    await expect(contest.connect(accounts[4]).claimFunds()).to.be.revertedWith("not a valid participant");
+
+    // try to claim funds before contest has ended
+    await expect(contest.connect(accounts[1]).claimFunds()).to.be.revertedWith("Game still active");
+
+    // claim funds successfully
+    let balanceBefore = await token.balanceOf(accounts[1].address)
+    await h.advanceTime((START_DEADLINE_DAYS + END_DEADLINE_DAYS) * 86400)
+    await contest.connect(accounts[1]).claimFunds()
+    let balanceAfter = await token.balanceOf(accounts[1].address)
+    // balance should equal wager + half wager of user who broke their streak (bc divied up to the remaining 2 ppl)
+    let expectedBalance = BigInt(WAGER) + BigInt(WAGER) / BigInt(2)
+    assert.equal(balanceAfter - balanceBefore, expectedBalance, "balance not correct")
+    
+    // try to claim funds again
+    await expect(contest.connect(accounts[1]).claimFunds()).to.be.revertedWith("funds already claimed");
+
+    // last eligible participant claims funds successfully
+    let balanceBefore2 = await token.balanceOf(accounts[2].address)
+    await contest.connect(accounts[2]).claimFunds()
+    let balanceAfter2 = await token.balanceOf(accounts[2].address)
+    assert.equal(balanceAfter2 - balanceBefore2, expectedBalance, "balance 2 not correct")
+
+    // participant who broke their streak tries to claim funds
+    await expect(contest.connect(accounts[3]).claimFunds()).to.be.revertedWith("not a valid participant");
+  });
+
+  it("getHandlesList", async function() {
+    handle1 = "bob"
+    handle2 = "alice"
+    handle3 = "ricky"
+    // fund accounts
+    bob = accounts[1]
+    alice = accounts[2]
+    ricky = accounts[3]
+    await token.faucet(bob.address)
+    await token.faucet(alice.address)
+    await token.faucet(ricky.address)
+    await token.connect(bob).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await token.connect(alice).approve(contest.address, WAGER + PROTOCOL_FEE)
+    await token.connect(ricky).approve(contest.address, WAGER + PROTOCOL_FEE)
+
+    // register
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 0, "handlesList should be empty")
+    await contest.connect(bob).register(handle1)
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 1, "handlesList should have 1 element")
+    assert.equal(handlesList[0], handle1, "handlesList should have correct handle")
+    await contest.connect(alice).register(handle2)
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 2, "handlesList should have 2 elements")
+    assert.equal(handlesList[0], handle1, "handlesList should have correct handle")
+    assert.equal(handlesList[1], handle2, "handlesList should have correct handle")
+    await contest.connect(ricky).register(handle3)
+    handlesList = await contest.getHandlesList()
+    assert.equal(handlesList.length, 3, "handlesList should have 3 elements")
+    assert.equal(handlesList[0], handle1, "handlesList should have correct handle")
+    assert.equal(handlesList[1], handle2, "handlesList should have correct handle")
+    assert.equal(handlesList[2], handle3, "handlesList should have correct handle")
+  })
+});
